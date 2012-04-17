5b0175625d312c15173503cec2f2b8be6618542d
Daniel Le <GreenRecycleBin@gmail.com>
2012-04-17 20:38:03 +0800

    Add solution to problem C "Recycled Numbers".

---
+++ 2012/Qualification-Round/c.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/ruby -w
+
+def permute(s)
+  permutation = [s.to_i]
+
+  (s.size - 1).times do
+    tmp = s[1..s.size - 1] << s[0]
+    permutation << tmp.to_i
+    s = tmp
+  end
+
+  permutation.uniq.sort
+end
+
+permutation_map = {}
+
+n = gets.chomp.to_i
+
+1.upto(n) do |id|
+  input = gets.chomp.split(' ')
+  a = input[0].to_i
+  b = input[1].to_i
+  
+  a.upto(b) do |i|
+    next if permutation_map[i] != nil
+
+    permutations = permute(i.to_s)
+    permutations.delete_if { |permutation| permutation < i }
+    permutation_map[i] = permutations
+    permutations.each { |x| permutation_map[x] = permutations }
+  end
+
+  sum = 0
+
+  a.upto(b) do |j|
+    permutations = permutation_map[j]
+    start_index = permutations.index(j)
+    end_index = permutations.index { |x| x > b }
+    end_index = permutations.size if end_index == nil
+    size = end_index - start_index
+    sum += size - 1 if size > 1
+  end
+
+  puts "Case ##{id}: #{sum}"
+end
