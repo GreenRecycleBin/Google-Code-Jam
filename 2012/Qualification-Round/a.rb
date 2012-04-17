05735556005a0094e18d842dad37cb5949188383
Daniel Le <GreenRecycleBin@gmail.com>
2012-04-17 20:37:20 +0800

    Add solution to problem A "Speaking in Tounges".

---
+++ 2012/Qualification-Round/a.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby -w
+
+char_to_char = {}
+
+input = 'abcdefghijklmnopqrstuvwxyz '
+output = 'yhesocvxduiglbkrztnwjpfmaq '
+
+0.upto(input.size - 1) do |i|
+  char_to_char[input[i].chr] = output[i].chr
+end
+
+n = STDIN.gets.chomp.to_i
+
+1.upto(n) do |id|  
+  line = STDIN.gets.chomp
+
+  print "Case #%d: " %[id]
+
+  line.each_char do |c|
+    print char_to_char[c]
+  end
+
+  puts ""
+end
