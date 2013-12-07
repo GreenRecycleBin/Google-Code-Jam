a1275372c7f465610dd561afd7eb8fb7aa818b37
Daniel Le <GreenRecycleBin@gmail.com>
2013-12-07 15:08:35 +0800

    Add a faster solution to problem "Theme Park"

--- 2010/qualification-round/ThemePark.java
+++ 2010/qualification-round/ThemePark.java
@@ -1,5 +1,7 @@
 import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -36,7 +38,8 @@ public class ThemePark {
         this.k = k;
         this.n = n;
         this.sizes = sizes;
-        profit = calculateProfit();
+        profit = r < n + 1 ? calculateProfit()
+            : calculateProfitWithCycleDetection();
     }
 
     private long calculateProfit() {
@@ -58,6 +61,73 @@ public class ThemePark {
             currentIndex = entry.getKey();
             profit += entry.getValue();
         }
+
+        return profit;
+    }
+
+    private long calculateProfitWithCycleDetection() {
+        // Mapping between a start position and the end position together with
+        // the number of people that can board the roller coaster
+        final Map<Integer, Long> numPeopleStartAt = new LinkedHashMap<>();
+
+        long profit = 0;
+        int currentIndex = 0;
+
+        // The queue can only have at most n states. Thus there will be a cycle
+        // after at most n + 1 rides
+        for (int i = 0; !numPeopleStartAt.containsKey(currentIndex)
+                 && i < n + 1; ++i) {
+            SimpleImmutableEntry<Integer, Long> entry =
+                getEndPositionAndNumPeopleStartAt(currentIndex);
+            numPeopleStartAt.put(currentIndex, entry.getValue());
+            currentIndex = entry.getKey();
+        }
+
+        final int cycleSize;
+        final int numRidesBeforeCycle;
+        long sumPeopleInCycle = 0;
+
+        {
+            int i = 0;
+
+            for (Iterator<Integer> it = numPeopleStartAt.keySet().iterator(); it
+                     .hasNext();) {
+                int startIndex = it.next();
+
+                if (startIndex == currentIndex) {
+                    sumPeopleInCycle += numPeopleStartAt.get(startIndex);
+
+                    while (it.hasNext()) {
+                        startIndex = it.next();
+                        sumPeopleInCycle += numPeopleStartAt.get(startIndex);
+                    }
+
+
+                    break;
+                } else {
+                    // Profit for the rides before the cycle
+                    profit += numPeopleStartAt.get(startIndex);
+                    ++i;
+                }
+            }
+
+            numRidesBeforeCycle = i;
+            cycleSize = numPeopleStartAt.size() - numRidesBeforeCycle;
+        }
+
+        final int numRidesInCycle = r - numRidesBeforeCycle;
+        final int numCycles = numRidesInCycle / cycleSize;
+        final int numRemainingCycles = numRidesInCycle % cycleSize;
+
+        profit += numCycles * sumPeopleInCycle;
+
+        Iterator<Integer> startOfCycleIt = numPeopleStartAt.keySet().iterator();
+
+        for (int i = 0; i < numRidesBeforeCycle; ++i) startOfCycleIt.next();
+
+        // Remaining profit for the remaining rides in the cycle
+        for (int i = 0; i < numRemainingCycles; ++i)
+            profit += numPeopleStartAt.get(startOfCycleIt.next());
 
         return profit;
     }
